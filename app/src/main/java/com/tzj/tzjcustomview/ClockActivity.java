c893620dfcc389f31613dcfefb9662c98bd3e97c
zhijieeeeee <tang562250920@gmail.com>
2016-11-07 14:25:15 +0800

    update

--- app/src/main/java/com/tzj/tzjcustomview/ClockActivity.java
+++ app/src/main/java/com/tzj/tzjcustomview/ClockActivity.java
@@ -3,6 +3,9 @@ package com.tzj.tzjcustomview;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import com.don.clockviewlibrary.ClockView;
 
 /**
  * <p>
@@ -13,9 +16,21 @@ import android.support.v7.app.AppCompatActivity;
  */
 public class ClockActivity extends AppCompatActivity {
 
+    private ClockView moveClockView;
+    private TextView tv;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_clock);
+        tv= (TextView) findViewById(R.id.tv);
+
+        moveClockView= (ClockView) findViewById(R.id.clockView);
+        moveClockView.setOnCurrentTimeListener(new ClockView.OnCurrentTimeListener() {
+            @Override
+            public void currentTime(String time) {
+                tv.setText(time);
+            }
+        });
     }
 }
