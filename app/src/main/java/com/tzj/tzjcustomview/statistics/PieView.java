d540e4c0391f83a4413aed39c52437356ea80d07
zhijieeeeee <tang562250920@gmail.com>
2016-11-25 14:33:47 +0800

    完成饼状图数据线绘制

--- app/src/main/java/com/tzj/tzjcustomview/statistics/PieView.java
+++ app/src/main/java/com/tzj/tzjcustomview/statistics/PieView.java
@@ -7,8 +7,8 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Rect;
 import android.graphics.RectF;
+import android.support.annotation.NonNull;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.View;
 
 import com.tzj.tzjcustomview.R;
@@ -31,10 +31,35 @@ public class PieView extends View {
     private static final int DEFAULT_HEIGHT = 800;
 
     /**
+     * 折线长度
+     */
+    private static final int SlASH_LINE_OFFSET = 60;
+
+    /**
+     * 横线长度
+     */
+    private static final int HOR_LINE_LENGTH = 180;
+
+    /**
+     * 横线上文字的横向偏移量
+     */
+    private static final int X_OFFSET = 20;
+
+    /**
+     * 横线上文字的纵向偏移量
+     */
+    private static final int Y_OFFSET = 10;
+
+    /**
      * 中心坐标
      */
     private int centerX;
     private int centerY;
+
+    /**
+     * 半径
+     */
+    private float radius;
 
     /**
      * 弧形外接矩形
@@ -44,7 +69,12 @@ public class PieView extends View {
     /**
      * 中间文本的大小
      */
-    private Rect textBound = new Rect();
+    private Rect centerTextBound = new Rect();
+
+    /**
+     * 数据文本的大小
+     */
+    private Rect dataTextBound = new Rect();
 
     /**
      * 扇形画笔
@@ -56,10 +86,17 @@ public class PieView extends View {
      */
     private Paint textPaint;
 
+    private Paint linePaint;
+
     /**
-     * 数据源数组
+     * 数据源数字数组
      */
     private int[] numbers;
+
+    /**
+     * 数据源名称数组
+     */
+    private String[] names;
 
     /**
      * 数据源总和
@@ -73,20 +110,34 @@ public class PieView extends View {
 
     private Random random = new Random();
 
+    //自定义属性 Start
+
     /**
      * 中间字体大小
      */
-    private float textSize = 20;
+    private float centerTextSize = 20;
+
+    /**
+     * 数据字体大小
+     */
+    private float dataTextSize = 10;
 
     /**
      * 中间字体颜色
      */
-    private int textColor = Color.BLACK;
+    private int centerTextColor = Color.BLACK;
+
+    /**
+     * 数据字体颜色
+     */
+    private int dataTextColor = Color.BLACK;
 
     /**
      * 圆圈的宽度
      */
     private float circleWidth = 50;
+
+    //自定义属性 End
 
     public PieView(Context context) {
         super(context);
@@ -100,9 +151,11 @@ public class PieView extends View {
     public PieView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
         TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.PieView);
-        textSize = typedArray.getDimension(R.styleable.PieView_textSize, textSize);
+        centerTextSize = typedArray.getDimension(R.styleable.PieView_centerTextSize, centerTextSize);
+        dataTextSize = typedArray.getDimension(R.styleable.PieView_dataTextSize, dataTextSize);
         circleWidth = typedArray.getDimension(R.styleable.PieView_circleWidth, circleWidth);
-        textColor = typedArray.getColor(R.styleable.PieView_textColor, textColor);
+        centerTextColor = typedArray.getColor(R.styleable.PieView_centerTextColor, centerTextColor);
+        dataTextColor = typedArray.getColor(R.styleable.PieView_dataTextColor, dataTextColor);
         typedArray.recycle();
         init();
     }
@@ -114,9 +167,15 @@ public class PieView extends View {
         mArcPaint.setStyle(Paint.Style.STROKE);
 
         textPaint = new Paint();
-        textPaint.setTextSize(textSize);
+        textPaint.setTextSize(centerTextSize);
         textPaint.setAntiAlias(true);
-        textPaint.setColor(textColor);
+        textPaint.setColor(centerTextColor);
+
+        linePaint = new Paint();
+        linePaint.setStrokeWidth(2);
+        linePaint.setTextSize(dataTextSize);
+        linePaint.setAntiAlias(true);
+        linePaint.setColor(dataTextColor);
     }
 
     @Override
@@ -142,7 +201,7 @@ public class PieView extends View {
         centerX = getMeasuredWidth() / 2;
         centerY = getMeasuredHeight() / 2;
         //设置半径为宽高最小值的1/4
-        float radius = Math.min(getMeasuredWidth(), getMeasuredHeight()) / 2 * 0.5f;
+        radius = Math.min(getMeasuredWidth(), getMeasuredHeight()) / 2 * 0.5f;
         //设置扇形外接矩形
         rectF = new RectF(centerX - radius,
                 centerY - radius,
@@ -157,13 +216,13 @@ public class PieView extends View {
     }
 
     /**
-     * 计算比例并且绘制扇形和数字
+     * 计算比例并且绘制扇形和数据
      */
     private void calculateAndDraw(Canvas canvas) {
         if (numbers == null || numbers.length == 0) {
             return;
         }
-        //开始度数
+        //扇形开始度数
         int startAngle = 0;
         for (int i = 0; i < numbers.length; i++) {
             //所占百分比
@@ -172,11 +231,139 @@ public class PieView extends View {
             float angle = (float) Math.ceil(percent * 360);
             //绘制第i段扇形
             drawArc(canvas, startAngle, angle, colors[i]);
-            Log.i("MyLog", "第" + i + "项：" + "startAngle=" + startAngle + ",angle=" + angle);
             startAngle += angle;
+
+            //绘制数据
+            if (numbers[i] <= 0) {
+                continue;
+            }
+            //当前扇形的中心度数
+            float arcCenterDegree = 90 + startAngle - angle / 2;
+            drawLineAndData(canvas, arcCenterDegree, i);
         }
         //绘制中心数字总和
-        canvas.drawText(sum + "", centerX - textBound.width() / 2, centerY + textBound.height() / 2, textPaint);
+        canvas.drawText(sum + "", centerX - centerTextBound.width() / 2, centerY + centerTextBound.height() / 2, textPaint);
+    }
+
+    /**
+     * 计算和绘制数据折线
+     *
+     * @param canvas 画布
+     * @param degree 当前扇形中心度数
+     * @param i      当前下标
+     */
+    private void drawLineAndData(Canvas canvas, float degree, int i) {
+        //扇形弧线中心点距离圆心的坐标
+        float x;
+        float y;
+        //折线开始坐标(扇形弧线中心点相对于view的坐标)
+        float startX;
+        float startY;
+        //折线结束坐标
+        float endX;
+        float endY;
+        //横线结束坐标
+        float horEndX;
+        float horEndY;
+        //数字开始坐标
+        float numberStartX;
+        float numberStartY;
+        //文本开始坐标
+        float textStartX;
+        float textStartY;
+
+        //sin 一二正，三四负
+        x = (float) (Math.sin(Math.toRadians(degree)) * radius);
+        //cos 一四正，二三负
+        y = (float) (Math.cos(Math.toRadians(degree)) * radius);
+
+        startX = centerX + x;
+        startY = centerY - y;
+
+        if (degree > 90 && degree < 180) {//2象限
+            endX = startX + SlASH_LINE_OFFSET;
+            endY = startY + SlASH_LINE_OFFSET;
+            //绘制折线
+            canvas.drawLine(startX, startY, endX, endY, linePaint);
+            //绘制横线
+            canvas.drawLine(endX, endY, endX + HOR_LINE_LENGTH, endY, linePaint);
+            //绘制数字
+            canvas.drawText(numbers[i] + "", endX + X_OFFSET, endY - Y_OFFSET, linePaint);
+            //绘制名称
+            canvas.drawText(names[i] + "", endX + X_OFFSET, endY + dataTextBound.height() / 2, linePaint);
+        } else if (degree == 180) {
+            startX = centerX;
+            startY = centerY + radius;
+            endX = startX + SlASH_LINE_OFFSET;
+            endY = startY + SlASH_LINE_OFFSET;
+            //绘制折线
+            canvas.drawLine(startX, startY, endX, endY, linePaint);
+            //绘制横线
+            canvas.drawLine(endX, endY, endX + HOR_LINE_LENGTH, endY, linePaint);
+            //绘制数字
+            canvas.drawText(numbers[i] + "", endX + X_OFFSET, endY - Y_OFFSET, linePaint);
+            //绘制名称
+            canvas.drawText(names[i] + "", endX + X_OFFSET, endY + dataTextBound.height() / 2, linePaint);
+        } else if (degree > 180 && degree < 270) {//3象限
+            endX = startX - SlASH_LINE_OFFSET;
+            endY = startY + SlASH_LINE_OFFSET;
+            //绘制折线
+            canvas.drawLine(startX, startY, endX, endY, linePaint);
+            //绘制横线
+            canvas.drawLine(endX, endY, endX - HOR_LINE_LENGTH, endY, linePaint);
+            //绘制数字
+            canvas.drawText(numbers[i] + "", endX - HOR_LINE_LENGTH + X_OFFSET, endY - Y_OFFSET, linePaint);
+            //绘制名称
+            canvas.drawText(names[i] + "", endX - HOR_LINE_LENGTH + X_OFFSET, endY + dataTextBound.height() / 2, linePaint);
+        } else if (degree == 270) {
+            startX = centerX - radius;
+            startY = centerY;
+            endX = startX - SlASH_LINE_OFFSET;
+            endY = startY - SlASH_LINE_OFFSET;
+            //绘制折线
+            canvas.drawLine(startX, startY, endX, endY, linePaint);
+            //绘制横线
+            canvas.drawLine(endX, endY, endX - HOR_LINE_LENGTH, endY, linePaint);
+            //绘制数字
+            canvas.drawText(numbers[i] + "", endX - HOR_LINE_LENGTH + X_OFFSET, endY - Y_OFFSET, linePaint);
+            //绘制名称
+            canvas.drawText(names[i] + "", endX - HOR_LINE_LENGTH + X_OFFSET, endY + dataTextBound.height() / 2, linePaint);
+        } else if (degree > 270 && degree < 360) {//4象限
+            endX = startX - SlASH_LINE_OFFSET;
+            endY = startY - SlASH_LINE_OFFSET;
+            //绘制折线
+            canvas.drawLine(startX, startY, endX, endY, linePaint);
+            //绘制横线
+            canvas.drawLine(endX, endY, endX - HOR_LINE_LENGTH, endY, linePaint);
+            //绘制数字
+            canvas.drawText(numbers[i] + "", endX - HOR_LINE_LENGTH + X_OFFSET, endY - Y_OFFSET, linePaint);
+            //绘制名称
+            canvas.drawText(names[i] + "", endX - HOR_LINE_LENGTH + X_OFFSET, endY + dataTextBound.height() / 2, linePaint);
+        } else if (degree == 360) {
+            startX = centerX;
+            startY = centerY - radius;
+            endX = startX - SlASH_LINE_OFFSET;
+            endY = startY - SlASH_LINE_OFFSET;
+            //绘制折线
+            canvas.drawLine(startX, startY, endX, endY, linePaint);
+            //绘制横线
+            canvas.drawLine(endX, endY, endX - HOR_LINE_LENGTH, endY, linePaint);
+            //绘制数字
+            canvas.drawText(numbers[i] + "", endX - HOR_LINE_LENGTH + X_OFFSET, endY - Y_OFFSET, linePaint);
+            //绘制名称
+            canvas.drawText(names[i] + "", endX - HOR_LINE_LENGTH + X_OFFSET, endY + dataTextBound.height() / 2, linePaint);
+        } else if (degree > 360) {//1象限
+            endX = startX + SlASH_LINE_OFFSET;
+            endY = startY - SlASH_LINE_OFFSET;
+            //绘制折线
+            canvas.drawLine(startX, startY, endX, endY, linePaint);
+            //绘制横线
+            canvas.drawLine(endX, endY, endX + HOR_LINE_LENGTH, endY, linePaint);
+            //绘制数字
+            canvas.drawText(numbers[i] + "", endX + X_OFFSET, endY - Y_OFFSET, linePaint);
+            //绘制名称
+            canvas.drawText(names[i] + "", endX + X_OFFSET, endY + dataTextBound.height() / 2, linePaint);
+        }
     }
 
     /**
@@ -207,15 +394,26 @@ public class PieView extends View {
      * 设置数据
      *
      * @param numbers 数字数组
+     * @param names   名称数组
      */
-    public void setData(int[] numbers) {
+    public void setData(@NonNull int[] numbers, @NonNull String[] names) {
+        if (numbers == null || numbers.length == 0 || names == null || names.length == 0) {
+            return;
+        }
+        if (numbers.length != names.length) {
+            //名称个数与数字个数不相等
+            return;
+        }
+
         this.numbers = numbers;
+        this.names = names;
         colors = new int[numbers.length];
         for (int i = 0; i < this.numbers.length; i++) {
             sum += numbers[i];
             colors[i] = randomColor();
         }
-        textPaint.getTextBounds(sum + "", 0, (sum + "").length(), textBound);
+        textPaint.getTextBounds(sum + "", 0, (sum + "").length(), centerTextBound);
+        textPaint.getTextBounds(names[0] + "", 0, (names[0] + "").length(), dataTextBound);
         invalidate();
     }
 }
--- app/src/main/java/com/tzj/tzjcustomview/statistics/StatisticsActivity.java
+++ app/src/main/java/com/tzj/tzjcustomview/statistics/StatisticsActivity.java
@@ -16,13 +16,14 @@ import com.tzj.tzjcustomview.R;
 public class StatisticsActivity extends AppCompatActivity {
 
     private PieView pieView;
-    private int[] data = new int[]{10, 20, 30, 40};
+    private int[] data = new int[]{10, 20,10};
+    private String[] name = new String[]{"监管查询", "执法巡查", "执法巡查"};
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_statistics);
         pieView = (PieView) findViewById(R.id.pieView);
-        pieView.setData(data);
+        pieView.setData(data, name);
     }
 }
