4693c7c531d9492565647c0417626b50a7804d84
zhijieeeeee <tang562250920@gmail.com>
2017-01-09 11:19:55 +0800

    添加PieView2测试

--- app/src/main/java/com/tzj/tzjcustomview/statistics/StatisticsActivity.java
+++ app/src/main/java/com/tzj/tzjcustomview/statistics/StatisticsActivity.java
@@ -5,6 +5,7 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
 import com.don.pieviewlibrary.PieView;
+import com.don.pieviewlibrary.PieView2;
 import com.tzj.tzjcustomview.R;
 
 /**
@@ -17,12 +18,12 @@ import com.tzj.tzjcustomview.R;
 public class StatisticsActivity extends AppCompatActivity {
 
     private PieView pieView;
-    private int[] data = new int[]{1,2,3,4,5};
-    private String[] name = new String[]{"监管查询1","监管查询2","监管查询3","监管查询4","监管查询5"};
+    private int[] data = new int[]{1, 2, 3, 4, 5};
+    private String[] name = new String[]{"监管查询1", "监管查询2", "监管查询3", "监管查询4", "监管查询5"};
 
-    private PieView pieView2;
-    private int[] data2 = new int[]{10,20,30};
-    private String[] name2 = new String[]{"北京","上海","深圳"};
+    private PieView2 pieView2;
+    private int[] data2 = new int[]{10, 20, 30, 20, 20};
+    private String[] name2 = new String[]{"北京", "上海", "深圳", "广州", "成都"};
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -31,7 +32,7 @@ public class StatisticsActivity extends AppCompatActivity {
         pieView = (PieView) findViewById(R.id.pieView);
         pieView.setData(data, name);
 
-        pieView2 = (PieView) findViewById(R.id.pieView2);
+        pieView2 = (PieView2) findViewById(R.id.pieView2);
         pieView2.setData(data2, name2);
     }
 }
