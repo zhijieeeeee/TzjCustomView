cdee5a37eadae4d16aa3532f82c201210a2b5310
zhijieeeeee <tang562250920@gmail.com>
2017-11-30 10:55:56 +0800

    update

--- app/src/main/java/com/tzj/tzjcustomview/html/HtmlActivity.java
+++ app/src/main/java/com/tzj/tzjcustomview/html/HtmlActivity.java
@@ -5,14 +5,20 @@ import android.os.PersistableBundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.text.Html;
+import android.text.TextUtils;
 import android.text.method.LinkMovementMethod;
 import android.util.Log;
 import android.view.View;
+import android.webkit.JavascriptInterface;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 import com.tzj.tzjcustomview.R;
+
+import jp.wasabeef.richeditor.RichEditor;
 
 /**
  * <p>
@@ -25,6 +31,7 @@ public class HtmlActivity extends AppCompatActivity {
 
     private EditText et;
     private Button btn;
+    private WebView wb;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -33,11 +40,15 @@ public class HtmlActivity extends AppCompatActivity {
 
         et = (EditText) findViewById(R.id.et);
         btn = (Button) findViewById(R.id.btn);
+        wb = (WebView) findViewById(R.id.wb);
 
         String html = "<b>哈哈哈哈</b><br/>" +
-                "<img src=\"https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=4131634322,487666839&fm=27&gp=0.jpg\" /><br>" +
-                "少数解耦爱江山哦i";
+                "<img src=\"https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=4131634322,487666839&fm=27&gp=0.jpg\" alt=\"11111\"/><br>" +
+                "<span class=\"fund_url\" fund-id =\"1916\">1916</span><fund>77777</fund>少数解耦爱江山哦i" +
+                "<span class=\"fund_url\" fund-id =\"210000\">210000</span>" +
+                "<span>没有class的</span>";
 
+        //TextView加载Html代码
         //设置超链接可以打开网页//click must
         et.setMovementMethod(LinkMovementMethod.getInstance());
         et.setText(Html.fromHtml(html, new URLImageGetter(et, this), new URLTagHandler(this)));
@@ -49,13 +60,73 @@ public class HtmlActivity extends AppCompatActivity {
                 int end = et.getSelectionEnd();
 //                CharSequence selectText = et.getText().subSequence(start, end);
                 Toast.makeText(HtmlActivity.this, Html.toHtml(et.getText()), Toast.LENGTH_SHORT).show();
-                Log.i("MyLog",Html.toHtml(et.getText()));
-                Log.i("MyLog","start="+start);
-                Log.i("MyLog","end="+end);
+                Log.i("MyLog", Html.toHtml(et.getText()));
+                Log.i("MyLog", "start=" + start);
+                Log.i("MyLog", "end=" + end);
 
-                String sss=Html.toHtml(et.getText());
+                String sss = Html.toHtml(et.getText());
                 et.setText(Html.fromHtml(sss, new URLImageGetter(et, HtmlActivity.this), new URLTagHandler(HtmlActivity.this)));
             }
         });
+
+
+        //使用WebView加载html源码，并且注入js和css
+
+        wb.getSettings().setJavaScriptEnabled(true);
+        wb.addJavascriptInterface(new JsCallJavaObj() {
+            @Override
+            @JavascriptInterface
+            public void toFundDetail(String fundId) {
+                if (!TextUtils.isEmpty(fundId)) {
+                    Toast.makeText(HtmlActivity.this, fundId, Toast.LENGTH_SHORT).show();
+                }
+            }
+        }, "jsCallJavaObj");
+        wb.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+                addImageClickListner();
+            }
+        });
+        //拼接css
+        String CSS_STYPE = "<head><style>img{max-width:100%  !important;}  " +
+                ".fund_url{color:#3d9dff}</style></head>";
+        String h = CSS_STYPE + html;
+        wb.loadData(CSS_STYPE + h, "text/html; charset=UTF-8", null);//这种写法可以正确解码
+
+    }
+
+    //注入js，并用js调用原生方法
+    private void addImageClickListner() {
+        wb.loadUrl("javascript:function img(){" +
+                "var href=document.getElementsByTagName(\"img\");" +
+                "\t\t for(var i=0;i<href.length;i++){\n" +
+                "\t\t \t   var a=href[i];\n" +
+                "\t\t \t   a.onclick=function(){\n" +
+                "\t\t \t        window.jsCallJavaObj.toFundDetail(this.alt)" +
+                "\t\t \t   };\n" +
+                "\t\t }" +
+                "}");
+        wb.loadUrl("javascript:function span(){" +
+                "var ttt=document.getElementsByTagName(\"span\");" +
+                "\t\t for(var i=0;i<ttt.length;i++){\n" +
+                "\t\t \t   var t=ttt[i];\n" +
+                "\t\t \t   t.onclick=function(){\n" +
+                "\t\t \t        window.jsCallJavaObj.toFundDetail(this.getAttribute('fund-id'))" +
+                "\t\t \t   };\n" +
+                "\t\t }" +
+                "}");
+        //执行js函数
+        wb.loadUrl("javascript:img()");
+        wb.loadUrl("javascript:span()");
+    }
+
+
+    /**
+     * Js調用Java接口
+     */
+    private interface JsCallJavaObj {
+        void toFundDetail(String fundId);
     }
 }
--- app/src/main/java/com/tzj/tzjcustomview/html/URLTagHandler.java
+++ app/src/main/java/com/tzj/tzjcustomview/html/URLTagHandler.java
@@ -4,13 +4,17 @@ import android.content.Context;
 import android.text.Editable;
 import android.text.Html;
 import android.text.Spanned;
+import android.text.TextUtils;
 import android.text.style.ClickableSpan;
 import android.text.style.ImageSpan;
+import android.text.style.StrikethroughSpan;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
 import org.xml.sax.XMLReader;
 
+import java.lang.reflect.Field;
 import java.util.Locale;
 
 /**
@@ -23,6 +27,10 @@ import java.util.Locale;
 public class URLTagHandler implements Html.TagHandler {
 
     private Context mContext;
+    int startTag;
+    int endTag;
+
+    String fundId = "";
 
     public URLTagHandler(Context context) {
         mContext = context.getApplicationContext();
@@ -40,6 +48,29 @@ public class URLTagHandler implements Html.TagHandler {
             // 使图片可点击并监听点击事件
             output.setSpan(new ClickableImage(mContext, imgURL), len - 1, len, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
         }
+        if (tag.toLowerCase(Locale.getDefault()).equals("span")) {
+            //opening是否是标签的开始，tag标签的名字，output输出的文字，xmlReader用来获取自定义属性。
+            if (opening) {
+                startTag = output.length();
+                fundId = startGame(tag, output, xmlReader);
+                Log.i("MyLog", "fundId: " + fundId);
+            } else {
+                endTag = output.length();
+                final String finalFundId = fundId;
+                Log.i("MyLog", "finalFundId: " + finalFundId);
+                if (!TextUtils.isEmpty(finalFundId)) {
+                    output.setSpan(new ClickableSpan() {
+                        @Override
+                        public void onClick(View widget) {
+                            Toast.makeText(mContext, finalFundId, Toast.LENGTH_SHORT).show();
+                        }
+                    }, startTag, endTag, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+                }
+            }
+        }
+
+        Log.i("MyLog", tag);
+
     }
 
     private class ClickableImage extends ClickableSpan {
@@ -56,6 +87,40 @@ public class URLTagHandler implements Html.TagHandler {
             // 进行图片点击之后的处理
             Toast.makeText(context, url, Toast.LENGTH_SHORT).show();
         }
+    }
+
+
+    public String startGame(String tag, Editable output, XMLReader xmlReader) {
+        try {
+            Field elementField = xmlReader.getClass().getDeclaredField("theNewElement");
+            elementField.setAccessible(true);
+            Object element = elementField.get(xmlReader);
+            Field attsField = element.getClass().getDeclaredField("theAtts");
+            attsField.setAccessible(true);
+            Object atts = attsField.get(element);
+            Field dataField = atts.getClass().getDeclaredField("data");
+            dataField.setAccessible(true);
+            String[] data = (String[]) dataField.get(atts);
+            Field lengthField = atts.getClass().getDeclaredField("length");
+            lengthField.setAccessible(true);
+            int len = (Integer) lengthField.get(atts);
+            String myAttributeA = null;
+            String myAttributeB = null;
+            for (int i = 0; i < len; i++) {
+                if ("class".equals(data[i * 5 + 1])) {
+                    myAttributeA = data[i * 5 + 4];
+                    Log.i("MyLog", "class: " + myAttributeA);
+                }
+                if ("fund-id".equals(data[i * 5 + 1])) {
+                    myAttributeB = data[i * 5 + 4];
+                    Log.i("MyLog", "fund-id: " + myAttributeB);
+                }
+            }
+            return myAttributeB;
+        } catch (NoSuchFieldException e) {
+        } catch (IllegalAccessException e) {
+        }
+        return null;
     }
 
 }
