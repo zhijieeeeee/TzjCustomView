67b65cc4f570977442a92391063da6829e0d3b55
zhijieeeeee <tang562250920@gmail.com>
2017-12-18 15:30:37 +0800

    添加view生成bitmap

--- app/src/main/java/com/tzj/tzjcustomview/verificationview/VerificationActivity.java
+++ app/src/main/java/com/tzj/tzjcustomview/verificationview/VerificationActivity.java
@@ -1,11 +1,14 @@
 package com.tzj.tzjcustomview.verificationview;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import com.tzj.tzjcustomview.R;
 
@@ -19,12 +22,14 @@ import com.tzj.tzjcustomview.R;
 public class VerificationActivity extends AppCompatActivity {
 
     VerificationView vv;
+    LinearLayout ll_container;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_verification);
 
+        ll_container= (LinearLayout) findViewById(R.id.ll_container);
         vv= (VerificationView) findViewById(R.id.vv);
         Button btnReset= (Button) findViewById(R.id.btnReset);
         btnReset.setOnClickListener(new View.OnClickListener() {
@@ -32,6 +37,16 @@ public class VerificationActivity extends AppCompatActivity {
             public void onClick(View v) {
                 vv.reset();
                 Log.i("MyLog","验证码是="+vv.getText());
+            }
+        });
+        Button btn_get= (Button) findViewById(R.id.btn_get);
+        final ImageView iv= (ImageView) findViewById(R.id.iv);
+        btn_get.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ll_container.buildDrawingCache();
+                Bitmap bitmap=ll_container.getDrawingCache();
+                iv.setImageBitmap(bitmap);
             }
         });
     }
